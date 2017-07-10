ae273386741a668d5bbb93ec48330a60b713aa56
Brian J. Walters <brian.joseph.walters@gmail.com>
2017-07-10 16:15:36 -0500

    Lab 11: Created Header component and PostList component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        PostListComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+            {
+                path: '',
+                redirectTo: 'posts',
+                pathMatch: 'full'
+            },
+            {
+                path: 'posts',
+                component: PostListComponent
+            }
+        ])
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
