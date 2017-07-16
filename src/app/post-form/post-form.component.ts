8e670138f8e425bf5a959fb794803f92e5e362c5
Brian J. Walters <brian.joseph.walters@gmail.com>
2017-07-15 20:14:07 -0500

    Lab 13: Bind data to the post form.

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Post } from '../post-service/post';
+
 @Component({
     selector: 'post-form',
     templateUrl: 'post-form.component.html',
     styleUrls: ['post-form.component.css']
 })
-export class PostFormComponent { }
+export class PostFormComponent {
+    private post: Post;
+
+    constructor() {
+        this.post = new Post();
+    }
+    
+}
